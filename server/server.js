4e3245fe6911315b865e4c796b12af19d8b7d2da
Kulathatguy <kulathatguy@gmail.com>
2023-12-03 02:32:49 -0500

    small change

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-app.get('/get-data/:id', (req, res) => {
+app.get('/get-data/:name', (req, res) => {
   const { name } = req.params;
 
   const params = {
@@ -43,52 +43,4 @@ app.get('/get-data/:id', (req, res) => {
   });
 });
 
-app.post('/put-data', express.json(), (req, res) => {
-  const { id, name, Pos, Age, Team, GamesPlayed, GamesStarted, MP, FG, FGA, FGPercent, ThreePoint, ThreePointAttempts, ThreePointPercent, TwoPoint, TwoPointAttempts, TwoPointPercent, effectiveField, FreeThrows, FreeThrowsAttempts, FreeThrowPercent, ORebound, DRebound, TRebound, Assists, Steals, Blocks, Turnovers, PersonalF, Points} = req.body;
-  
-  const params = {
-    TableName: 'Test',
-    Item: {
-      'id': id,
-      'name': name,
-      'Pos': Pos,
-      'Age': Age,
-      'Team': Team,
-      'Games Played': GamesPlayed,
-      'Games Started': GamesStarted,
-      'MP': MP,
-      'FG': FG,
-      'FGA': FGA,
-      'FG%': FGPercent,
-      '3P': ThreePoint,     
-      '3PA': ThreePointAttempts,   
-      '3P%': ThreePointPercent,    
-      '2P': TwoPoint,     
-      '2PA': TwoPointAttempts,    
-      '2P%': TwoPointPercent,    
-      'eFG%': effectiveField,   
-      'FT': FreeThrows,     
-      'FTA': FreeThrowsAttempts,    
-      'FT%': FreeThrowPercent,    
-      'ORB': ORebound,    
-      'DRB': DRebound,    
-      'TRB': TRebound,    
-      'AST': Assists,   
-      'STL': Steals,    
-      'BLK': Blocks,    
-      'TOV': Turnovers,    
-      'PF': PersonalF,     
-      'PTS': Points,    
-    },
-  };
 
-  ddb.put(params, (err, data) => {
-    if (err) {
-      console.error("Error", err);
-      res.status(500).json({ error: 'Error inserting data into DynamoDB' });
-    } else {
-      console.log("Success", data);
-      res.json({ success: true });
-    }
-  });
-});
