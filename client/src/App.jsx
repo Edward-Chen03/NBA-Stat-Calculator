58c0695e37512a6713bbb7d8c71530d29aba16ae
Marcus Lau <108362808+BabyDinos@users.noreply.github.com>
2023-12-03 22:44:03 -0500

    added pagination to the table

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,15 @@ function App() {
 
   return (
     <>
-    <div className="flex bg-gray-500 justify-center align-middle items-center w-screen h-screen">
+    <div className="flex bg-gray-500 justify-center align-middle items-center w-full h-screen">
       <h1 className="text-white text-5xl">Hello</h1>
     </div>
-    <Table></Table>
+    <div className="bg-gray-500 w-full h-screen">
+      <div className="mx-5 pt-52">
+        <Table></Table>
+      </div>
+    </div>
+
     </>
   )
 }
--- client/src/components/Table/Table.jsx
+++ client/src/components/Table/Table.jsx
@@ -2,45 +2,75 @@
 import React, {useState} from 'react'
 import TableRow from "./TableRow.jsx"
 import TableHeader from "./TableHeader.jsx";
+import data from "../../data.json"
+
+
+// Eventually pagination will be implemented where a GET request ON already sorted data will forgo the need to import all the data, then sort it.  
 
 function Table() {
+    
+    // data and data formatting
+    const header = data["header"]
+    const arrayOfJSON = data["data"]
+    let arrangedArrayOfData = arrayOfJSON
+    .filter(dataItem => header.some(headerItem => Object.keys(dataItem).includes(headerItem)))
+    .map(dataItem => header.map(headerItem => dataItem[headerItem]));
 
-    let data = [["Eddie", "Marcus' House", "35", "2", "Me"], ["Marcus", "Eddie's House", "", "5", ""]]
-    let header = ["Player", "Team", "Points", "Rebounds", "Placeholder"]
+    // pages
+    const rowsPerPage = 10
+    const [pageNumber, setPageNumber] = useState(1)
+    const handlePageChange = (newPage) => {
+        if (newPage > 0 && newPage <= totalPages) {
+            setPageNumber(newPage);
+        }
+    };
+    let firstIndex = (pageNumber-1) * rowsPerPage
+    let lastIndex = firstIndex + rowsPerPage + 1
+
+    // sorting algo for columns
     const [sortConfig, setSortConfig] = useState({column: null, direction:'asc'})
-
     const handleSort = (column) => {
         setSortConfig({
           key: column,
           direction: sortConfig.key === column && sortConfig.direction === 'asc' ? 'desc' : 'asc',
         });
       };
-  
-    const sortedData = [...data].sort((a, b) => {
-        if (sortConfig.key) {
-            const index = header.indexOf(sortConfig.key);
-            const keyA = a[index];
-            const keyB = b[index];
+    let sortedData = [...arrangedArrayOfData].sort((a, b) => {
+    if (sortConfig.key) {
+        const index = header.indexOf(sortConfig.key);
+        const keyA = a[index];
+        const keyB = b[index];
+        console.log(keyA)
 
-            if (sortConfig.direction === 'asc') {
-                return keyA.localeCompare(keyB, undefined, { numeric: true, sensitivity: 'base' });
-            } else {
-                return keyB.localeCompare(keyA, undefined, { numeric: true, sensitivity: 'base' });
-            }
+        if (sortConfig.direction === 'asc') {
+            return keyA > keyB ? 1 : -1;
+        } else {
+            return keyA < keyB ? 1 : -1;
         }
-    
-        return 0;
-      });
+    }
+
+    return 0;
+    });
+
+
+    const totalPages = Math.ceil(sortedData.length / 10)
+    let paginatedData = sortedData.slice(firstIndex, lastIndex)
+
 
     return (
         <>
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead className="text-xs justify-between text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <div className="flex flex-col w-full shadow-md bg-gray-500">
+            <div className="flex flex-row justify-end text-xs items-end text-gray-700 uppercase bg-gray-50">
+                <button className='px-6 py-3' onClick={() => handlePageChange(pageNumber - 1)}>Previous</button>
+                <div className='text-black w-6 px-1 py-1 self-center text-center'>{pageNumber}/{totalPages}</div>
+                <button className='px-6 py-3' onClick={() => handlePageChange(pageNumber + 1)}>Next</button>
+            </div>
+            <table className="w-full h-fit text-sm text-left rtl:text-right text-gray-500">
+                <thead className="text-xs justify-between text-gray-700 uppercase bg-gray-50">
                     <TableHeader header_data={header} sortConfig={sortConfig} onSort={handleSort}></TableHeader>
                 </thead>
                 <tbody>
-                    {sortedData.map((item, index) => (
+                    {paginatedData.map((item, index) => (
                         <TableRow key={index} data={item}></TableRow>
                     ))}
                 </tbody>
