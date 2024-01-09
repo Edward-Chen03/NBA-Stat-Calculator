1dad5cde93a61d8d93accfca3aff3b12f693ce8b
Kulathatguy <kulathatguy@gmail.com>
2024-01-08 22:50:03 -0500

    route changes

--- client/server.js
+++ client/server.js
@@ -26,11 +26,11 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-app.use(express.static(path.join(__dirname, '../src')));
+app.use(express.static(path.join(__dirname, 'src')));
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../src', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 
